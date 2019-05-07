cf90532798b2b49dd3aa82e21af7e233328039a8
Jack Hawkins <jack.hawkins77@googlemail.com>
2019-05-07 10:05:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,6 +4,8 @@ def reverse_each_word(sentence)
   
   new_array = []
   
-  new_array = word_array.collect{|word|word
+  new_array = word_array.collect{|word|word.reverse}
+  
+  new_array.join(" ")
 
 end
